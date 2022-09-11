4081efc0c8f0c61815434edb895b1751a3c0cf76
RCHAUHANN <97589219+RCHAUHANN@users.noreply.github.com>
2022-09-11 22:31:30 +0300

    for loops

---
+++ for.py
@@ -0,0 +1,59 @@
+#1
+import random
+
+
+num_dice = int(input("how many dice you want to roll?"))
+sum_dice=0
+
+for sum_dice in range (num_dice):
+    print('dice:',sum_dice ,'number:', random.randint(1, 6))
+    sum_dice +=1
+
+#2
+#num= int(input('enter a number: '))
+#input_list=[]
+#input_list.append(num)
+#while True:
+   # if num =="":
+       # break
+   # else:
+        #num = int(input('enter a number: '))
+    #input_list.append(num)
+    #input_list.sort(reverse=True)
+
+#print(input_list)
+
+
+#3
+n= int(input("enter a number to check prime:"))
+answer= False
+if n>1:
+    for i in  range (2,n//2):
+        if n%i==0:
+            answer= True
+            break
+
+if answer:
+    print(n,"is not a prime number.")
+else:
+    print(n, "is a prime number.")
+
+#4
+city =[]
+for i in range(5):
+   city_name= input("enter the name of a city: ")
+   city.append(city_name)
+
+for x in range (len(city)):
+    print(city[x])
+
+
+
+
+
+
+
+
+
+
+
