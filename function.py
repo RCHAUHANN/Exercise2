9c6df95c1b5f872a40e9718437e35c7248b6c4f0
RCHAUHANN <97589219+RCHAUHANN@users.noreply.github.com>
2022-09-13 11:43:54 +0300

    functions

--- function.py
+++ function.py
@@ -4,25 +4,47 @@ import random
 def dice_num():
     dice_roll = random.randint(1, 6)
     return dice_roll
+value= dice_num()
+while value<=5:
+    print("the number is:",value)
 
-while dice_num()<=5:
-    print("the number is:",dice_num())
-    if dice_num()==6:
-     break
+    if value==6:
+        print("the number is 6!!!")
+        break
+    value=dice_num()
+print(value)
 
+
+#2
+def dice_num(sides):
+    dice_roll = random.randint(1, sides)
+    return dice_roll
+blah = int(input("number pof sides?"))
+value= dice_num(blah)
+while value< blah:
+    print(value)
+
+    if value==blah:
+
+        break
+    value=dice_num(blah)
+print(value)
 #3
 def gallons(n):
     litres= n*3.785
+    print(f"Amount in litres {litres:.2f}")
     return litres
 
 
-value = float(input("enter value in gallons: "))
 
-while value>0:
+
+while True:
     value = float(input("enter value in gallons: "))
-    print=("the value in litres is: ",gallons(value))
+
+
     if value<0:
         break
+    gallons(value)
 
 
 #4
@@ -38,41 +60,44 @@ print(sum(list))
 
 #5
 def list(list1):
-    list1 = [2, 4, 19, 13, 42, 6, 65]
+
     for num in list1:
-        if num % 2 == 0:
-            print(num, end=" ")
-            return list1
+        if num % 2 != 0:
+            list1.remove(num)
+
+    return list1
 
 
-list1 = [2, 4, 19, 13, 42, 6, 65]
+list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 print(list1)
-(list(list1))
+potato = list(list1)
+print(potato)
 
 
 #6
 import math
 def pizza(diameter,euro):
-    diameter= float(input("please enter the diameter of the pizza: "))
-    euro= float(input("please enter the price of the pizza: "))
+
 
     r = diameter/2
-    area = (math.pi * r**2)
+    area = (math.pi * r**2)*0.0001
     result = euro/area
     print(f"the cost per square inch is {result:.2f}  euros")
 
     return result
 
-pizza()
+diameter1= float(input("please enter the diameter of the pizza: "))
+euro1= float(input("please enter the price of the pizza: "))
 
 diameter2= float(input("please enter the diameter of other pizza:"))
 euro2 = float(input("please enter the price of other pizza: "))
-pizza(diameter2,euro2)
 
-if pizza()> pizza(diameter2,euro2):
+
+if pizza(diameter1,euro1)> pizza(diameter2,euro2):
     print("the other pizza is cheaper")
 else:
     print("the first pizza is cheaper")
+
 
 
 
